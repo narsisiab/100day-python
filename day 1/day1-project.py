7569221dd20b76ee471c2e6da7ab9b64bccc6397
narsisiab <narsisiab@gmail.com>
2026-02-19 21:13:14 +0330

    first commit - day 1 python

---
+++ day 1/day1-project.py
@@ -0,0 +1,4 @@
+print("welcome to the  band name generator")
+city = input("what is the name of the city you grew up in?\n")
+pet = input(" what is your pet name?\n")
+print(" your band name could be " + city + " " + pet)
---
+++ day 1/day1.py
@@ -0,0 +1,5 @@
+# print("hello " + input("what is your name?")+"!")
+
+username = input("what is your name?")
+length = len(username)
+print(length)
